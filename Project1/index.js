c2eb756b14031e93a3fc9cc8a4e5028c1087b9d9
Souravroy99 <souravroy200418@gmail.com>
2026-08-11 23:53:05 +0530

    Implement basic redis methods in project1

--- Project1/index.js
+++ Project1/index.js
@@ -4,13 +4,37 @@ dotenv.config()
 import express from "express"
 import connectDB from "./lib/db.js"
 import User from "./model/user.model.js"
+import Redis from "ioredis"
 
 const app = express()
+
+// Usage of redis
+const redis = new Redis(process.env.REDIS_URL)
+
+
 app.use(express.json())
 
+// Without redis
 app.get('/', (req, res) => {
     return res.status(201).json({message: "Hello from Redis Project_1"})
 })
+
+// With redis
+app.get('/get-with-redis', async(req, res) => {
+
+    const cached = await redis.get("user:all")
+
+    if(cached) {
+        const user = JSON.parse(cached) 
+        return res.status(200).json(user) 
+    }
+
+    const user = await User.find()
+    await redis.set("user:all", JSON.stringify(user))
+
+    return res.status(200).json(user)
+})
+
 
 app.get('/get', async (req, res) => {
     const user = await User.find()
@@ -19,6 +43,9 @@ app.get('/get', async (req, res) => {
 
 app.post('/create', async (req, res) => {
     const {name, email, password} = req.body
+
+    // Whenever we create new data, we need to clear the cached data.
+    await redis.del("user:all")
 
     const user = await User.create({name, email, password})
 
--- Project1/model/user.model.js
+++ Project1/model/user.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String
-}, {timestamps: true})
+}, { timestamps: true })
 
 const User = await mongoose.model("User", userSchema)
 
